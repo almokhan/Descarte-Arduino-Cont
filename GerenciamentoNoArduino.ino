693995328fb248d5ae325993c54a9f79b33431c7
almokhan <118139242+almokhan@users.noreply.github.com>
2024-08-30 09:48:06 -0300

    Do jeito que vai funcionar
    Agora o Arduino esta recebendo <rele,1> para abrir a portinhola e <rele,0> para fechar. Ele não interage com os sensores e só tem uma unica função.

--- GerenciamentoNoArduino.ino
+++ GerenciamentoNoArduino.ino
@@ -1,33 +1,9 @@
 int rele1 = 13;      // Pino do relé
-int sensorObj = 12;      //Pino do sensor de Objetos
-int sensorPort = 11;      //Pino do sensor da porta
 String comando = ""; // Buffer para armazenar a string recebida
 
 void setup() {
   pinMode(rele1, OUTPUT);
-  pinMode(sensorObj, INPUT);
-  pinMode(sensorPort, INPUT);
   Serial.begin(9600);
-}
-
-void descarte(){  //Responsavel por um descarte seguro
-  digitalWrite(rele1, HIGH);
-  while (digitalRead(sensorPort) == LOW){ //Trava o programa até a Portinhola abrir
-    digitalWrite(rele1, HIGH);
-  }
-  while(digitalRead(sensorObj) == LOW){  //Trava o programa até o sensor registra algo
-    delay(8);  //Serve para evitar um possivel bouncing
-  }
-  while(digitalRead(sensorObj) == HIGH){  //Trava o programa até o sensor ser liberado
-    delay(8);
-  }
-  delay(2000); //Tempo de segurança para a portinhola fechar
-  digitalWrite(rele1, LOW);
-  while(digitalRead(sensorPort) == HIGH){ //Trava o programa até a portinhola ser fechada
-    digitalWrite(rele1, LOW);
-  Serial.write(1);
-  }
-
 }
 
 void loop() {
@@ -43,8 +19,11 @@ void loop() {
       String comandoLimpo = comando;
       comandoLimpo.remove(comandoLimpo.length() - 1); // Remove '>'
       if (comandoLimpo == "<rele,1") {
-        descarte();
+        Serial.println("Ligado");
+        digitalWrite(rele1, HIGH); // Ativa o relé
       } else if (comandoLimpo == "<rele,0") {
+        digitalWrite(rele1, LOW);  // Desativa o relé
+        Serial.println("Desligado");
       } else {
       }     
       comando = "";
